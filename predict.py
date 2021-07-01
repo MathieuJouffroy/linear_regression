ad1ac271c6773cd02c8ec30584fec628c69270c5
MathieuJouffroy <mathieu.jouffroy@gmail.com>
2021-07-01 17:35:30 +0200

    Refactoring : added docstrings

--- predict.py
+++ predict.py
@@ -1,59 +1,70 @@
-from utils import get_thetas, get_data, feature_scaling_normalization, feature_scaling_standardization
-from linear_regression import LinearRegression
-import matplotlib.pyplot as plt
 import sys
 import argparse
+import matplotlib.pyplot as plt
+from lin_regression import LinearRegression
+from utils import get_data, get_thetas_and_fs_method, feature_scaling_normalization, feature_scaling_standardization
+
 
 def get_mileage():
-	while (1):
-		try:
-			mileage = input(f'Enter a mileage: ')
-		except EOFError:
-			sys.exit('EOF on input. Exit..')
-		except:
-			sys.exit('Error on input. Exit.')
-		try:
-			mileage = float(mileage)
-			if mileage >= 0:
-				break
-			else:
-				print(f'Cannot predict the price for a negative mileage, input a positive number.\n')
-		except ValueError:
-			print (f'Input for mileage not valid, try again.\n')
-	return (mileage)
+    """ Prompt the user to input a mileage and returns the result as a float. """
+    while (1):
+        try:
+            mileage = input(f'Enter a mileage: ')
+        except EOFError:
+            sys.exit('EOF on input. Exit..')
+        except:
+            sys.exit('Error on input. Exit.')
+        try:
+            mileage = float(mileage)
+            if mileage >= 0:
+                break
+            else:
+                print(f'Cannot predict the price for a negative mileage, input a positive number.\n')
+        except ValueError:
+            print (f'Input for mileage not valid, try again.\n')
+    return (mileage)
 
-def feature_scaling_input(args, mileages, mileage):
-	if args.feature_scaling == 'normalization':
-		x_min = min(mileages)
-		x_max = max(mileages)
-		mean = sum(mileages) / len(mileages)
-		fs_mileage = (mileage - mean) / (x_max - x_min)
-	elif args.feature_scaling == 'standardization':
-		mean = sum(mileages) / len(mileages)
-		variance = sum([((xi - mean) ** 2) for xi in mileages]) / len(mileages)
-		std = variance ** 0.5
-		fs_mileage = (mileage - mean) / std
-	return (fs_mileage)
+def feature_scaling_input(method, mileages, mileage):
+    """
+    Returns the feature scaled mileage. It is very important that the feature 
+    scaling method used is the same as the one used for training. 
+
+    Arguments:
+    args -- argument parser containing the method used for the computations
+    method -- feature scaling method used for training
+    mileages -- input variable (list of floats)
+    mileage -- user input (int)
+    
+    Returns:
+    fs_mileage -- feature scaled mileage
+    """
+    
+    if method == 'normalization':
+        x_min = min(mileages)
+        x_max = max(mileages)
+        mean = sum(mileages) / len(mileages)
+        fs_mileage = (mileage - mean) / (x_max - x_min)
+    elif method == 'standardization':
+        mean = sum(mileages) / len(mileages)
+        variance = sum([((xi - mean) ** 2) for xi in mileages]) / len(mileages)
+        std = variance ** 0.5
+        fs_mileage = (mileage - mean) / std
+    return (fs_mileage)
 
 def main():
-	parser = argparse.ArgumentParser(description='LR Gradient Descent')
-	parser.add_argument('--feature_scaling', choices={"normalization", "standardization"}, help='Feature scaling method: Can either be normalization or standardization')
-	args = parser.parse_args()
-
-	mileages, price = get_data('./data.csv')
-	theta = get_thetas('model.csv')
-	mileage = get_mileage()
-	model = LinearRegression()
-	model.set_weigths(theta)
-
-	if args.feature_scaling:
-		mileage = feature_scaling_input(args, mileages, mileage)
-
-	price = model.predict_(mileage)
-	if price < 0:
-		print(f'This car has no more value ({price:.3f}).')
-	else:
-		print(f'Estimated price: {price:.3f} $')
+    mileages, price = get_data('./data.csv')
+    theta, method = get_thetas_and_fs_method('model.csv')
+    mileage = get_mileage()
+    mileage = feature_scaling_input(method, mileages, mileage)
+    
+    model = LinearRegression()
+    model.set_weigths(theta)
+    price = model.predict_(mileage)
+    
+    if price < 0:
+        print(f'This car has no more value ({price:.3f}).')
+    else:
+        print(f'Estimated price: {price:.3f} $')
 
 if __name__ == "__main__":
-	main()
+    main()
--- utils.py
+++ utils.py
@@ -1,81 +1,133 @@
-import numpy as np
-import csv
 import os
-from typing import List, Tuple
+import csv
+import numpy as np
 
 def get_data(dataset):
-	mileage = []
-	price = []
-	if (os.path.isfile(dataset)):
-		with open(dataset, 'r') as csvfile:
-			reader = csv.reader(csvfile, delimiter=',')
-			# You cannot loop over the reader more than once (it is a generator)
-			# cannot do multiple list comprehension on a generator without re-declaring it.
-			for row in reader:
-				mileage.append(row[0])
-				price.append(row[1])
-		mileage.pop(0)
-		price.pop(0)
-		# Ensure both list are correct with eval
-		for i in range(len(mileage)):
-			mileage[i] = eval(mileage[i])
-			price[i] = eval(price[i])
-	return mileage, price
+    """
+    Returns the input variable and the output variable from our dataset.
+    The input variable is the 1st column and the output variable is the 2nd column.
+    Note that you cannot loop over the reader more than once (it is a generator).
+    You cannot do multiple list comprehension on a generator without re-declaring it.
+    
+    Arguments:
+    dataset -- csv file containing the dataset
+    
+    Returns:
+    mileage -- list of int (input variable)
+    price -- list of int (output variable)
+    """
 
-def	get_thetas(model):
-	theta = []
-	if (os.path.isfile(model)):
-		with open(model, 'r') as csvfile:
-			file = csv.reader(csvfile, delimiter=',')
-			for row in file:
-				theta = [float(row[0]), float(row[1])]
-				break
-	else:
-		theta = [0.0, 0.0]
-	return (theta)
+    mileage = []
+    price = []
+    if (os.path.isfile(dataset)):
+        with open(dataset, 'r') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            for row in reader:
+                mileage.append(row[0])
+                price.append(row[1])
+        mileage.pop(0)
+        price.pop(0)
+        for i in range(len(mileage)):
+            mileage[i] = eval(mileage[i])
+            price[i] = eval(price[i])
+    return mileage, price
+
+def	get_thetas_and_fs_method(model):
+    """
+    Returns the weights (weight and biais) theta and the method used
+    for feature scaling our features. The weights where updated while fitting
+    our model. Note that we should used the same feature scaling method for
+    the training and for the prediction.
+
+    Arguments:
+    model -- LinearRegression model (Class) 
+    
+    Returns:
+    theta -- θ₀,θ₁ : Biais and Weight 
+    fs_method -- feature scaling method
+    """
+
+    theta = []
+    if (os.path.isfile(model)):
+        with open(model, 'r') as csvfile:
+            file = csv.reader(csvfile, delimiter=',')
+            for row in file:
+                theta = [float(row[0]), float(row[1])]
+                fs_method = row[4]
+    else:
+        theta = [0.0, 0.0]
+        fs_method = 'normalization'
+    return (theta, fs_method)
 
 def feature_scaling_normalization(args, X):
-	if args.method == 'vectorization':
-		x_min = X.min()
-		x_max = X.max()
-		mean = X.mean()
-		X = (X - mean) / (x_max - x_min)
-	elif args.method == 'for_loop':
-		x = []
-		x_min = min(X)
-		x_max = max(X)
-		mean = sum(X) / len(X)
-		for xi in X:
-			x.append((xi - mean) / (x_max - x_min))
-		X = x
-	return X
+    """
+    Returns the normalized input variable X.
+
+    Arguments:
+    args -- argument parser containing the method used for the computations
+    X -- input variable
+    
+    Returns:
+    X -- normalized input variable
+    """
+
+    if args.method == 'vectorization':
+        x_min = X.min()
+        x_max = X.max()
+        mean = X.mean()
+        X = (X - mean) / (x_max - x_min)
+    elif args.method == 'for_loop':
+        x = []
+        x_min = min(X)
+        x_max = max(X)
+        mean = sum(X) / len(X)
+        for xi in X:
+            x.append((xi - mean) / (x_max - x_min))
+        X = x
+    return X
 
 def	feature_scaling_standardization(args, X):
-	if args.method == 'vectorization':
-		mean = X.mean()
-		std = X.std()
-		X = (X - mean) / std
-	elif args.method == 'for_loop':
-		x = []
-		mean = sum(X) / len(X)
-		variance = sum([((xi - mean) ** 2) for xi in X]) / len(X)
-		std = variance ** 0.5
-		for xi in X:
-			x.append((xi - mean) / std)
-		X = x
-	return X
+    """
+    Returns the standardized input variable X.
 
-def save_model(theta, alpha, n_iters, file='./model.csv'):
-	theta0, theta1 = np.float(theta[0]), np.float(theta[1])
-	if os.path.isfile(file):
-		print ("append")
-		with open(file, 'a+') as f:
-			writer = csv.writer(f)
-			writer.writerow([theta0, theta1, alpha, n_iters])
-			f.close()
-	else:
-		print ("write")
-		with open(file, 'w+') as f:
-			writer = csv.writer(f, delimiter=',')
-			writer.writerow([theta0, theta1, alpha, n_iters])
-			f.close()
+    Arguments:
+    args -- argument parser containing the method used for the computations
+    X -- input variable
+    
+    Returns:
+    X -- standardized input variable
+    """
+
+    if args.method == 'vectorization':
+        mean = X.mean()
+        std = X.std()
+        X = (X - mean) / std
+    elif args.method == 'for_loop':
+        x = []
+        mean = sum(X) / len(X)
+        variance = sum([((xi - mean) ** 2) for xi in X]) / len(X)
+        std = variance ** 0.5
+        for xi in X:
+            x.append((xi - mean) / std)
+        X = x
+    return X
+
+def save_model(theta, alpha, n_iters, method, file='./model.csv'):
+    """
+    Saves the model in a CSV file containing 5 columns.
+    The first 2 columns are the biais and the weights. The 3rd column
+    is the learning rate alpha, the 4th column is the number of iterations.
+    And the last column is the feature scaling method used for training.
+
+    Arguments:
+    theta -- θ₀,θ₁ : Biais and Weight after fitting the model
+    alpha -- learning rate used for training
+    n_iters -- number of iterations for the gradient descent
+    method -- feature scaling method used for training.
+    """
+
+    theta0, theta1 = np.float(theta[0]), np.float(theta[1])
+    with open(file, 'w+') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow([theta0, theta1, alpha, n_iters, method])
+        f.close()
