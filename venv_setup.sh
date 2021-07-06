1ea1492a8000491777b29fae7be0e09ec0bb7ad1
MathieuJouffroy <mathieu.jouffroy@gmail.com>
2021-07-06 16:49:39 +0200

    refacto

--- venv_setup.sh
+++ venv_setup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 # -m option: run library module as script
-# pip defaults to installing Python packages to a 
-# system directory (such as /usr/local/lib/python3.4). 
-# This requires root access.
-# --user makes pip install packages in your home directory
-# instead, which doesn't require any special privileges.
-# install pip manually to ensure having the latest version
-# it's recommended to uses the system pip to bootstrap
-# a user installation
-# https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/
+# --user makes pip install packages in home directory (no special privileges)
+
 python3 -m pip install --user --upgrade pip
 python3 -m pip --version
 python --version
@@ -20,7 +13,7 @@ python3 -m venv v_env
 # Activate this environment’s packages/resources in isolation 
 source v_env/bin/activate
 
-# Inside the script at bin/activate, there's a line that looks 
+# Inside bin/activate, there's a line that looks 
 # like this: VIRTUAL_ENV="/Users/me/.envs/myenv"
 if [[ "$VIRTUAL_ENV" != "" ]]; then
     echo "Your virtual environment was created!"
